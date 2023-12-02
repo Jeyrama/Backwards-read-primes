c29afc6f1b3c271ba2ef48188a4d919302ab080f
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-02 12:30:25 -0800

    Update Backwards-read-primes.js

--- Backwards-read-primes.js
+++ Backwards-read-primes.js
@@ -47,3 +47,45 @@ const backwardsPrime = (start, stop) => {
 }
 
 // or
+
+function backwardsPrime(start, stop) {
+  let range = [];
+  for (let i = start % 2 == 0 ? start + 1 : start; i <= stop; i += 2) range.push(i);
+  return range.filter(function(v) {return isPrime(v);}).filter(function(v) {
+    let rev = parseInt(v.toString().split('').reverse().join(''));
+    return rev != v && validPrimeCheck(rev);
+  });
+}
+
+//Uses an ever-expanding Sieve of Eratosthenes to test for primes.
+let sieve = [2,3,5,7,11,13,17,19,23,29,31,37];
+
+function validPrimeCheck(num) {
+  generateSieve(Math.ceil(Math.sqrt(num)));
+  return isPrimeFromSieve(num);
+}
+
+function generateSieve(max) {
+  let current = sieve[sieve.length - 1] + 2;
+  while (current <= max) {
+    if (isPrimeFromSieve(current)) sieve.push(current);
+    current += 2;
+  }
+}
+
+function isPrimeFromSieve(num) {
+  let max = Math.ceil(Math.sqrt(num));
+  for (let i = 0; i < sieve.length; i++) {
+    if (num % sieve[i] === 0) return false;
+    else if (max < sieve[i]) return true;
+  }
+  return true;
+}
+
+function isPrime(num) {
+  if (num < 2 || (num % 2 === 0 && num !== 2)) return false;
+  else {
+    if (sieve.indexOf(num) !== -1) return true;
+    else return validPrimeCheck(num);
+  }
+}
